602ddb968ca40c703d3dca174e0906be70635d89
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2013-11-04 21:41:11 +0100

    Working PrimeBank with test, begin create primeCount Actor

--- src/main/scala/pl/brosbit/Boot.scala
+++ src/main/scala/pl/brosbit/Boot.scala
@@ -2,17 +2,15 @@ package pl.brosbit
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 
-case class Greeting(who: String)
-
-class GreetingActor extends Actor with ActorLogging {
-    def receive = {
-        case Greeting(who) ⇒ log.info("Hello " + who)
-    }
-}
 object Boot extends App {
     
     val system = ActorSystem("MySystem")
-    val greeter = system.actorOf(Props[GreetingActor], name = "greeter")
-    greeter ! Greeting("Charlie Parker")
+    val primeCounter = system.actorOf(Props[PrimeCounterActor], name = "PrimeCounter")
+    var i = 2
+    while (i < Long.MaxValue) {
+      primeCounter ! i
+      i += 1
+    }
+    
 }
 
---
+++ src/main/scala/pl/brosbit/PrimeCounterActor.scala
@@ -0,0 +1,20 @@
+package pl.brosbit
+
+import akka.actor.{Actor, ActorLogging}
+
+class PrimeCounterActor extends Actor with ActorLogging {
+  val primeBank = new PrimeBank(10)
+  
+  def receive= {
+    case numb:Int => {
+      val n = primeBank.check(numb)
+      if(n != 0L) {
+        sender ! n
+        log.info("State " + printBank)
+      }
+      
+    }
+  }
+  
+  def printBank = println(primeBank.bank.mkString(", "))
+}
