de2fe88f8b45848a11325226353d2dfaf47ba730
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2013-11-06 16:34:05 +0100

    remove mainactor

--- src/main/scala/pl/brosbit/Boot.scala
+++ src/main/scala/pl/brosbit/Boot.scala
@@ -2,13 +2,41 @@ package pl.brosbit
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 
-case class StartCount()
+case class PrintPrimes()
 
-object Boot extends App {
+object Boot  extends App {
     
+    val primeBank = new PrimeBank(100)
     val system = ActorSystem("MySystem")
-    val mainActor = system.actorOf(Props[MainActor], name = "MainCounterActor")
-    mainActor ! StartCount()
+    val nextActor = system.actorOf(Props[PrimeCounterActor], name = "primeActor")
+    println("Begins Loop")
+    var i = 2L
+    while (i < 250000L) {
+      val n = primeBank.check(i)
+      if(n != 0L) nextActor ! n
+      i += 1L
+      Thread.sleep(10)
+    }
+    println("Ends Loop")
+    nextActor ! PrintPrimes()
+   
+    Thread.sleep(1000)
+    println("To end 4s")
+    
+    Thread.sleep(1000)
+    println("To end 3s")
+    
+    Thread.sleep(1000)
+    println("To end 2s")
+    
+    Thread.sleep(1000)
+    println("To end 1s")
+    
+    Thread.sleep(1000)
+    println("close!")
+    system.shutdown
+    
+  def printBank = println(primeBank.bank.mkString(", "))
     
 }
 
--- src/main/scala/pl/brosbit/PrimeCounterActor.scala
+++ src/main/scala/pl/brosbit/PrimeCounterActor.scala
@@ -1,22 +1,32 @@
 package pl.brosbit
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorLogging, ActorSystem, Props}
 
 class PrimeCounterActor extends Actor with ActorLogging {
-  val primeBank = new PrimeBank(1000)
-  val system = ActorSystem("MySystem")
-  val nextCounter = system.actorOf(Props[PrimeCounterActor], name = "PrimeCounter")
+  val primeBank = new PrimeBank(100)
+  import context._
+  var nextCounter:ActorRef = null
   
   def receive= {
     case numb:Int => {
-      val n = primeBank.check(numb)
+      val n = primeBank.check(numb)  
+    		  
       if(n != 0L) {
+        if(nextCounter == null) {
+          nextCounter = actorOf(Props[PrimeCounterActor], name = "PrimeCounter")
+          log.info("created new Prime Actor")
+        }
         nextCounter ! n
         log.info("State " + printBank)
       }
       
     }
+    case p:PrintPrimes => {
+      printBank
+      if(nextCounter != null) nextCounter ! PrintPrimes()
+      stop(self)
+    }
   }
   
-  def printBank = println(primeBank.bank.mkString(", "))
+  def printBank = log.info(primeBank.bank.mkString(", "))
 }
