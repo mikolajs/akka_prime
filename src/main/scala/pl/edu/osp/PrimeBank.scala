193958df105e89b808fb5c19860d693efe5c0798
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2016-02-01 13:36:55 +0100

    repair bank

--- src/main/scala/pl/edu/osp/PrimeBank.scala
+++ src/main/scala/pl/edu/osp/PrimeBank.scala
@@ -1,16 +1,16 @@
 package pl.edu.osp
 
 class PrimeBank (size:Int) {
-	var bank:List[Long] = List()
+	v var bank:scala.collection.mutable.ListBuffer[Long] = scala.collection.mutable.ListBuffer()
 
   //if not full return 0L, else it's prime and can't store
   def check(numb: Long): Long = {
     if (bank.isEmpty) {
-      bank = numb :: bank
+      bank += numb 
       return 0L
     } else if (!foundFactorial(numb)) {
       if ((bank.head * bank.head) > numb && bank.size < size) {
-        bank = numb :: bank
+        bank += numb
         return 0L
       } else numb
     } else 0L
@@ -19,6 +19,7 @@ class PrimeBank (size:Int) {
 	
 	private def foundFactorial(numb:Long):Boolean = {
 	  for(bNumb <- bank){
+        if(bNumb * bNumb > numb) return false
 	    if(numb % bNumb == 0) return true
 	  }
 	  return false
